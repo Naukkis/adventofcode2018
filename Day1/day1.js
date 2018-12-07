8f19e81578e705f410b17090167330a9e81e4f62
Naukkis <naukkis@gmail.com>
2018-12-07 22:40:46 +0200

    day1

---
+++ Day1/day1.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+let changes = fs.readFileSync("input.txt").toString();
+changes = changes.split("\n");
+
+const changesAsIntegers = changes.map(x => x.substring(0,1) === '+' ? parseInt(x.substring(1)) : parseInt(x)).slice(0, changes.length-1);
+
+let firstDuplicateFound = false;
+let frequencySet = new Set([0]);
+
+console.time('dumdum');
+let current = 0;
+while(!firstDuplicateFound) {
+    for (let i = 0; i < changesAsIntegers.length; i++) {
+        current += changesAsIntegers[i];
+        if (frequencySet.has(current)) {
+            console.log(current);
+            firstDuplicateFound = true;
+            break;
+        }else {
+            frequencySet.add(current);
+        }
+    }
+}
+console.timeEnd('dumdum');
